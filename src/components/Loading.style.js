52ea0aac3aa7ce9b06b4af33f6a32e5b56f3d196
HarunHatiboglu <harun.hatiboglu@helloworld.com.tr>
2023-08-20 02:17:47 +0300

    Add commit Loading components

---
+++ src/components/Loading.style.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    alignSlef: 'center',
+    padding: 12,
+    borderRadius: 10,
+    borderColor: '#e0e0e0e',
+    alignSelf: 'center',
+    bottom: '50%',
+  },
+});
