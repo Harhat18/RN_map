e83229370d093fa0b1519f4f6313e635f52f337e
HarunHatiboglu <harun.hatiboglu@helloworld.com.tr>
2023-08-20 02:17:16 +0300

    add get data with useFetch

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 import React from 'react';
 import MapView from 'react-native-maps';
+import useFetch from './src/hooks/useFetch';
+import Loading from './src/components/Loading';
 
+const url = 'https://random-data-api.com/api/v2/users?size=20';
 const App = () => {
+  const {data, loading, error} = useFetch(url);
+  console.log({data, loading, error});
   return (
     <SafeAreaView style={{flex: 1}}>
-      <MapView style={{flex: 1}} />
+      <MapView provider="google" style={{flex: 1}} />
+      {/* <View style={{backgroundColor: 'red', position: 'absolute', top: 50}}>
+        <Text>Test</Text>
+      </View> */}
+      <Loading />
     </SafeAreaView>
   );
 };
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,23 +1,23 @@
-import {useState, useEffect, useCallback} from 'react';
-import {axios} from 'axios';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 export default function useFetch(url) {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const fetchData = useCallback(async () => {
-    try {
-      const {data: response} = await axios.get(url);
-      setData(response);
-      setLoading(false);
-    } catch (err) {
-      setError(err);
-      setLoading(false);
-    }
-  }, [url]);
+  const [error, setError] = useState(true);
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchData();
-  }, [fetchData]);
+  }, [url]);
   return {data, loading, error};
 }
